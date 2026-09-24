f27fe2348731ca234dc0309c9e77abb94b706bb4
foshare183-wq <foshare183@gmail.com>
2026-09-24 12:55:47 +0900

    Delete assets directory

--- assets/js/dflip-config.js
+++
@@ -1,7 +0,0 @@
-// Konfigurasi flipbook DearFlip. Harus dimuat SEBELUM dflip.min.js
-window.DFLIP = window.DFLIP || {};
-window.DFLIP.defaults = {
-    webgl: false,
-    soundEnable: true,
-    hideControls: "download,print,more,share,fullscreen,toggle,play",
-};
